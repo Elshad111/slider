a1a2cee83893868e210c2d7e49b22d033ef8ddcf
Elshad <safarov.elshad007@gmail.com>
2019-01-13 20:03:09 +0500

    Created class slider

--- js/slider.js
+++ js/slider.js
@@ -1,3 +1,19 @@
 window.onload = function(){
-	
+	let slider = new Slider({
+		container: 'container',
+		item: 1
+	});
+}
+class Slider{
+	constructor(options){
+		let defaultOptions = {
+			container: 'slider',
+			item: 'slider__item',
+			prev: 'slider__control-left',
+			next: 'slider__control-right',
+		};
+		for(let option in defaultOptions){
+			this[option] = (options && options[option] !== undefined) ? options[option] : defaultOptions[option];
+		}
+	}
 }
