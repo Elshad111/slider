5951567dbf69140e6d2d884e8c2191b955235e5c
Elshad <safarov.elshad007@gmail.com>
2019-01-14 18:20:55 +0500

    Added method sliderInit

--- js/slider.js
+++ js/slider.js
@@ -1,16 +1,27 @@
-window.onload = function(){
-	
-}
+"use strict";
+
 class Slider{
 	constructor(options){
 		let defaultOptions = {
-			wrapper: 'slider',
-			item: 'slider__item',
-			prev: 'slider__control-left',
-			next: 'slider__control-right',
+			wrapper: '.slider__wrapper',
+			items: '.slider__item',
+			prev: '.slider__control-left',
+			next: '.slider__control-right'
 		};
+		this.i = 0;
 		for(let option in defaultOptions){
 			this[option] = (options && options[option] !== undefined) ? options[option] : defaultOptions[option];
 		}
 	}
+	sliderInit(){
+		this.nextSlide(this.options, this.i);
+	}
 }
+
+const slider = new Slider({
+	wrapper: '.slider__wrapper',
+	items: '.slider__item',
+	prev: '.slider__control-left',
+	next: '.slider__control-right',
+});
+slider.sliderInit();
