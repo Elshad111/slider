d1bf06c0614b1bca350df7cbfc8947a47655ddfb
Elshad <safarov.elshad007@gmail.com>
2019-01-14 18:36:02 +0500

    Added prevSlide method

--- js/slider.js
+++ js/slider.js
@@ -15,18 +15,31 @@ class Slider{
 	}
 	sliderInit(){
 		this.nextSlide(this.options, this.i);
+		this.prevSlide(this.options, this.i);
 	}
 }
 
 Slider.prototype.nextSlide = function(options, i){
 	let next = document.querySelector(this.next);
 	let items = document.querySelectorAll(this.items);
-	let wrapper = document.querySelector(this.wrapper);
 	next.addEventListener('click', function(){
 		items[i].classList.remove('show');
 		i++;
 		if(i > items.length - 1){
 			i = 0;
+		}
+		items[i].classList.add('show');
+	});
+}
+
+Slider.prototype.prevSlide = function(options, i){
+	let prev = document.querySelector(this.prev);
+	let items = document.querySelectorAll(this.items);
+	prev.addEventListener('click', function(){
+		items[i].classList.remove('show');
+		i--;
+		if(i < 0){
+			i = items.length - 1;
 		}
 		items[i].classList.add('show');
 	});
