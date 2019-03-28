4090d5008fa06a9b8209d3e8e836f551ea1d2e79
Elshad <safarov.elshad007@gmail.com>
2019-03-28 21:10:08 +0500

    Changed gulpfile.js file

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,8 @@ const pngquant = require('imagemin-pngquant');
 const imagemin = require('gulp-imagemin'); 
 const cache = require('gulp-cache');
 const plumber = require('gulp-plumber');
+const del = require('del');
+const browserSync = require('browser-sync').create();
 
 const cssFile = [
 	'css/normalize.css',
@@ -18,8 +20,8 @@ const cssFile = [
 ];
 
 const copyFile = [
-	'*.html',
-	'*.ico'
+	'index.html',
+	'favicon.ico'
 ];
 
 gulp.task('styles', () => {
@@ -32,12 +34,13 @@ gulp.task('styles', () => {
 				.pipe(cleanCSS({
 					level: 2
 				}))
-				.pipe(gulp.dest('build/css'));
+				.pipe(gulp.dest('build/css'))
+				.pipe(browserSync.stream());;
 });
 
 gulp.task('copy-html', () => {
 	return gulp.src(copyFile)
-				.pipe(gulp.dest('build'));
+				.pipe(gulp.dest('build')).pipe(browserSync.stream());;
 });
 
 gulp.task('script', () => {
@@ -45,7 +48,7 @@ gulp.task('script', () => {
 				.pipe(sourcemaps.init())
 				.pipe(rollup({}, 'iife'))
 				.pipe(sourcemaps.write())
-				.pipe(gulp.dest('build/js'));
+				.pipe(gulp.dest('build/js')).pipe(browserSync.stream());;
 });
 
 gulp.task('clear', function (done) {
@@ -55,5 +58,28 @@ gulp.task('clear', function (done) {
 gulp.task('imagemin', () => {
 	return gulp.src('img/*')
 				.pipe(imagemin())
-				.pipe(gulp.dest('build/img'));
+				.pipe(gulp.dest('build/img')).pipe(browserSync.stream());;
 });
+
+gulp.task('clean', () => {
+	return del(['build/*']);
+});
+
+gulp.task('watch', () => {
+	browserSync.init({
+        server: {
+            baseDir: "./build",
+       },
+       notify: false
+    });
+
+	gulp.watch('css/**/*.css', gulp.series('styles'));
+	gulp.watch('js/**/*.js', gulp.series('script'));
+	gulp.watch('index.html', gulp.series('copy-html'));
+	gulp.watch('img/*', gulp.series('imagemin'));
+});
+
+gulp.task('build', gulp.series('clean', 
+						gulp.parallel('styles', 'script', 'copy-html', 'imagemin')));
+
+gulp.task('dev', gulp.series('build', 'watch'));
