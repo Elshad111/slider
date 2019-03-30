dd59b332ddfa0f4db70fc552d5893b4ee7c1cc28
Elshad <safarov.elshad007@gmail.com>
2019-03-31 00:07:14 +0500

    Changed slider.js file

--- js/slider.js
+++ js/slider.js
@@ -3,30 +3,31 @@ export default class Slider{
 		this.items = document.querySelectorAll(`${obj.wrapper} ${obj.items}`);
 		this.prev = document.querySelector(`${obj.wrapper} ${obj.prev}`);
 		this.next = document.querySelector(`${obj.wrapper} ${obj.next}`);
-		this._index = 0;		
+		this._count = 0;		
 		this.next.addEventListener('click', () => {
 			this.nextSlide();
 		});
 		this.prev.addEventListener('click', () => {
 			this.prevSlide();
-		});
+		}); 
 	}
 
 	nextSlide() {
-		this.items[this._index].classList.remove('show');
-		this._index++;
-		if(this._index > this.items.length - 1){
-			this._index = 0;
+		this.items[this._count].classList.remove('show');
+		this._count++;
+		if(this._count > this.items.length - 1){
+			this._count = 0;
 		}
-		this.items[this._index].classList.add('show');
+		this.items[this._count].classList.add('show');
 	}
 
 	prevSlide() {
-		this.items[this._index].classList.remove('show');
-		this._index--;
-		if(this._index < 0){
-			this._index = this.items.length - 1;
+		this.items[this._count].classList.remove('show');
+		this._count--;
+		if(this._count < 0){
+			this._count = this.items.length - 1;
 		}
-		this.items[this._index].classList.add('show');
+		this.items[this._count].classList.add('show');
 	}
+	
 }
